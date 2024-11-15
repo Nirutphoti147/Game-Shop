77b49594b68d8b5b5aadb76596198a3dee7e0303
Nirutphoti147 <Nirutphoti147@gmail.com>
2024-11-15 11:30:38 +0700

    LAST

--- scripts.js
+++ scripts.js
@@ -1,53 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameList = document.getElementById('game-list');
     const games = [
-        { 
-            name: 'Pac-Man', 
-            description: 'Classic arcade game.', 
-            imageUrl: 'img/pacman.png', 
-            price: '$19.99' 
+        {
+            name: 'Pac-Man',
+            description: 'Classic arcade game.',
+            imageUrl: 'img/pacman.png',
+            price: '$19.99'
         },
-        { 
-            name: 'Space Invaders', 
-            description: 'Defend against alien invaders.', 
-            imageUrl: 'img/space-invaders.jpg', 
-            price: '$14.99' 
+        {
+            name: 'Space Invaders',
+            description: 'Defend against alien invaders.',
+            imageUrl: 'img/space-invaders.jpg',
+            price: '$14.99'
         },
-        { 
-            name: 'Donkey Kong', 
-            description: 'Help Mario save the princess.', 
-            imageUrl: 'img/donkey-kong.jpg', 
-            price: '$24.99' 
+        {
+            name: 'Donkey Kong',
+            description: 'Help Mario save the princess.',
+            imageUrl: 'img/donkey-kong.jpg',
+            price: '$24.99'
         },
-        { 
-            name: 'Super Mario Bros', 
-            description: 'Join Mario on a classic adventure.', 
-            imageUrl: 'img/super-mario.jpg', 
-            price: '$29.99' 
+        {
+            name: 'Super Mario Bros',
+            description: 'Join Mario on a classic adventure.',
+            imageUrl: 'img/super-mario.jpg',
+            price: '$29.99'
         },
-        { 
-            name: 'Tetris', 
-            description: 'The classic puzzle game.', 
-            imageUrl: 'img/tetris.png', 
-            price: '$9.99' 
+        {
+            name: 'Tetris',
+            description: 'The classic puzzle game.',
+            imageUrl: 'img/tetris.png',
+            price: '$9.99'
         },
-        { 
-            name: 'Street Fighter II', 
-            description: 'The ultimate fighting game.', 
-            imageUrl: 'img/street-fighter.jpg', 
-            price: '$19.99' 
+        {
+            name: 'Street Fighter II',
+            description: 'The ultimate fighting game.',
+            imageUrl: 'img/street-fighter.jpg',
+            price: '$19.99'
         },
-        { 
-            name: 'Galactic Quest: The Lost Stars', 
-            description: 'Recover the lost stars of the galaxy.', 
-            imageUrl: 'img/galactic-quest.jpg', 
-            price: '$21.99' 
+        {
+            name: 'Galactic Quest: The Lost Stars',
+            description: 'Recover the lost stars of the galaxy.',
+            imageUrl: 'img/galactic-quest.jpg',
+            price: '$21.99'
         },
-        { 
-            name: 'X-Men: Mutant Apocalypse', 
-            description: 'Join the X-Men in their battle against evil mutants.', 
-            imageUrl: 'img/x-men-mutant-apocalypse.jpg', 
-            price: '$22.99' 
+        {
+            name: 'X-Men: Mutant Apocalypse',
+            description: 'Join the X-Men in their battle against evil mutants.',
+            imageUrl: 'img/x-men-mutant-apocalypse.jpg',
+            price: '$22.99'
         }
     ];
 
